a01b6875e7dc126486ae639e644b648baed2efc5
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-25 22:38:11 +0200

    work

---
+++ emails.R
@@ -0,0 +1,93 @@
+require(blastula)
+
+img_string <- add_image(file = "selo_obs_h.png", 250)
+
+send_email <- function(email, subject){
+  smtp_send(
+    email = email,
+    to = "rfsaldanha@gmail.com",
+    from = "raphael.saldanha@fiocruz.br",
+    subject = subject,
+    credentials = creds_file("smtp2go_creds"),
+    verbose = FALSE
+  )
+}
+
+send_email_database_error <- function(e, context){
+  date_time <- add_readable_time()
+
+  email <- compose_email(
+    body = md(glue::glue(
+        "{img_string}
+        Olá,
+
+        Ocorreu um erro na conexão com o banco de dados.
+
+        Contexto: {context}
+
+        -----
+
+        {e}
+
+        -----
+
+        
+        ")),
+    footer = md(glue::glue("{date_time}."))
+  )
+
+  send_email(email, "Erro na conexão com o banco de dados")
+}
+
+
+send_email_data_retrieve_error <- function(e, context){
+  date_time <- add_readable_time()
+
+  email <- compose_email(
+    body = md(glue::glue(
+        "{img_string}
+        Olá,
+
+        Não foi possível buscar os dados da estação.
+
+        Contexto: {context}
+
+        -----
+          
+        {e}
+
+        -----
+
+        
+        ")),
+    footer = md(glue::glue("{date_time}."))
+  )
+
+  send_email(email, "Erro no acesso de dados da estação")
+}
+
+send_email_write_db_error <- function(e, context){
+  date_time <- add_readable_time()
+
+  email <- compose_email(
+    body = md(glue::glue(
+        "{img_string}
+        Olá,
+
+        Não foi possível escrever os dados do sensor no banco de dados.
+
+        Contexto: {context}
+
+        -----
+          
+        {e}
+
+        -----
+
+        
+        ")),
+    footer = md(glue::glue("{date_time}."))
+  )
+
+  send_email(email, "Erro ao escrever os dados da estação no banco de dados")
+}
---
+++ weatherlink_job.R
@@ -0,0 +1,127 @@
+# Packages
+library(weatherlinkapi)
+library(duckdb)
+library(lubridate)
+library(dplyr)
+library(cli)
+library(rlang)
+library(blastula)
+library(glue)
+source("emails.R")
+
+# Message and keep job start timestamp
+cli_alert_info("Job start: {now()}")
+now_ts <- format(now(tzone = "Brazil/East"), "%d/%m/%Y %H:%M:%S")
+
+# Database connection
+con <- tryCatch(
+  {
+    dbConnect(duckdb(), "weatherlink.duckdb")
+  }, 
+  error=function(e) {
+    cli_alert_warning("Could not connect to database.")
+    message(e)
+    send_email_database_error(e, "Conexão com o banco de dados local da WeatherLink")
+    cli_abort("This update was aborted.")
+  }
+)
+
+# Station ids
+station_ids <- c(195669)
+
+# Empty database creation. Keep it commented!!
+# for(d in station_ids){
+#   res <- current_data(d)
+#   for(s in 1:length(res)){
+#     table_name <- paste0("station_",d,"_sensor_",res[[s]]$lsid)
+
+#     empty_table <- res[[s]][0,]
+
+#     dbWriteTable(
+#       conn = con, 
+#       name = table_name, 
+#       value = empty_table
+#     )
+#   }
+# }
+
+# For each station...
+for(d in station_ids){
+  # Retrieve data from station
+  cli_alert("Retrieving data from station {d}...")
+  res <- tryCatch(
+    {
+      current_data(d)
+    }, 
+    error=function(e) {
+      cli_alert_warning("Could not retrieve data from station {d}.")
+      message(e)
+      send_email_data_retrieve_error(e, glue("Estação {d} da WeatherLink"))
+      cli_abort("This update was aborted.")
+    }
+  )
+  cli_alert_success("Data from station {d} retrieved successfully.")
+  
+  # Write to database
+  for(s in 1:length(res)){
+    table_name <- paste0("station_",d,"_sensor_",res[[s]]$lsid)
+
+    # Check if data was already written
+    last_update_file_name <- paste0("weatherlink_last_update_",table_name,".rds")
+    update_database <- NA
+    
+    if(!file.exists(last_update_file_name)){
+      update_database <- TRUE
+    } else {
+      last_update_data <- readRDS(last_update_file_name)
+
+      if(hash(last_update_data) == hash(res[[s]])){
+        update_database <- FALSE
+      } else {
+        update_database <- TRUE
+      }
+    }
+
+    if(!update_database){
+      cli_alert_danger("The current data from from station {d}, sensor {s} is the same of the last update. This sensor update will be skipped.")
+      next
+    } else {
+      # Write to database
+      cli_alert("Writing new data from device {d}, sensor {res[[s]]$lsid} to database...")
+      db_write <- tryCatch(
+        {
+          dbWriteTable(
+            conn = con, 
+            name = table_name, 
+            value = res[[s]], 
+            append = TRUE
+          )
+        }, 
+        error=function(e) {
+          cli_alert_warning("Could not write data from station {d}, sensor {s} to database.")
+          message(e)
+          send_email_write_db_error(e, glue("Estação {d}, sensor {s} da WeatherLink"))
+          cli_abort("This update was aborted.")
+        }
+      )
+
+      # Save rds file
+      saveRDS(
+        object = res[[s]],
+        file = paste0("weatherlink_last_update_",table_name,".rds")
+      )
+        
+      cli_alert_success("Done.")
+    }
+  }
+}
+
+# Disconnect from database
+dbDisconnect(con)
+
+# Save last end time
+saveRDS(object = now_ts, file = "last_update_time.rds")
+
+# Final messages
+cli_alert_info("End of update.")
+cli_alert_info("Job end: {now()}")
