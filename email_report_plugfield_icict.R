adb339ce01ea536016e1c46d82ab99dc6c5d8377
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-02 14:00:12 +0200

    All recipients set

--- email_report_plugfield_icict.R
+++ email_report_plugfield_icict.R
@@ -17,12 +17,12 @@ schema <- dbplyr::in_schema("estacoes", "tb_estacao_1b")
 img_string <- add_image(file = "ws_job/selo_obs_h.png", 250)
 recipients <- c(
   "raphael.saldanha@fiocruz.br"
-  #"diego.ricardo@fiocruz.br",
-  #"vanderlei.pascoal@fiocruz.br",
-  #"heglaucio.barros@fiocruz.br",
-  #"renata.gracie@fiocruz.br",
-  #"christovam.barcellos@fiocruz.br",
-  #"izabio2005@gmail.com"
+  "diego.ricardo@fiocruz.br",
+  "vanderlei.pascoal@fiocruz.br",
+  "heglaucio.barros@fiocruz.br",
+  "renata.gracie@fiocruz.br",
+  "christovam.barcellos@fiocruz.br",
+  "izabio2005@gmail.com"
 )
 
 # Plugfield
