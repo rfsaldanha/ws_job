4031d75cb4b465047e8e26fb5bef4dfe70d9fb8a
Raphael Saldanha <rfsaldanha@gmail.com>
2024-10-02 10:41:35 +0200

    Remove Y axis limit

--- email_report_weatherlink_icict.R
+++ email_report_weatherlink_icict.R
@@ -141,8 +141,7 @@ plot_chuva <- ggplot(data = res_chuva, aes(x = time, y = value)) +
   geom_line() + 
   labs(title = "Chuva", x = "Data", y = "mm") +
   theme_bw() +
-  scale_x_datetime(date_labels = "%b %d", date_breaks = "1 day") +
-  expand_limits(y=0)
+  scale_x_datetime(date_labels = "%b %d", date_breaks = "1 day")  
 
 plot_chuva <- add_ggplot(plot_chuva, width = 7, height = 5)
 
@@ -166,8 +165,7 @@ plot_vento <- ggplot(data = res_vento, aes(x = time, y = value)) +
   labs(title = "Vento", x = "Data", y = "mm", color = NULL) +
   theme_bw() +
   theme(legend.position = "bottom", legend.direction = "horizontal") +
-  scale_x_datetime(date_labels = "%b %d", date_breaks = "1 day") +
-  expand_limits(y=0)
+  scale_x_datetime(date_labels = "%b %d", date_breaks = "1 day")  
 
 plot_vento <- add_ggplot(plot_vento, width = 7, height = 5)
 
