abf5b403ad56512a46479ef1959f7d31656563ef
rfsaldanha <rfsaldanha@gmail.com>
2026-01-14 14:49:50 -0300

    bug fix

--- plugfield_job_psql_icict.R
+++ plugfield_job_psql_icict.R
@@ -80,28 +80,29 @@ for (d in device_ids) {
   res <- tibble()
 
   # For each sensor...
+  if (d == 4863) {
+    sensor_ids <- c(8, 35, 36, 37, 11, 18, 19, 22, 27, 28, 34, 23, 25, 26, 1)
+  } else if (d == 10611) {
+    sensor_ids <- c(
+      8,
+      35,
+      348,
+      48,
+      347,
+      11,
+      18,
+      19,
+      22,
+      27,
+      28,
+      49,
+      23,
+      25,
+      1
+    )
+  }
+
   for (s in sensor_ids) {
-    if (d == 4863) {
-      sensor_ids <- c(8, 35, 36, 37, 11, 18, 19, 22, 27, 28, 34, 23, 25, 26, 1)
-    } else if (d == 10611) {
-      sensor_ids <- c(
-        8,
-        35,
-        348,
-        48,
-        347,
-        11,
-        18,
-        19,
-        22,
-        27,
-        28,
-        49,
-        23,
-        25,
-        1
-      )
-    }
     cli_alert("Retrieving data from station {d}, sensor {s}...")
     tmp <- tryCatch(
       {
